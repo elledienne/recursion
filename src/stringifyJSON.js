cfc4d58c96a19db7f0d5c2f2eabd9dd0767abc40
Lorenzo De Nobili <elledienne@gmail.com>
2015-09-19 10:53:28 +0200

    Implemented JSONstringify

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -2,7 +2,42 @@
 // var stringifyJSON = JSON.stringify;
 
 // but you don't so you're going to write it from scratch:
+var each = function(collection, callback) {
+  if(Array.isArray(collection)) {
+    for(var i = 0; i < collection.length; i++) {
+      callback(collection[i], i, collection);
+    }
+  } else if(typeof collection === 'object') {
+    for(var key in collection) {
+      callback(collection[key], key, collection);
+    }
+  } else {
+    callback(collection);
+  }
+}
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  var objType = typeof obj;
+
+  if(objType === 'function' || objType === 'undefined') {
+    return undefined;
+  } else if(objType !== 'object' || obj === null){
+    if(objType === 'string') {
+      return '"' + obj + '"';
+    }
+    return '' + obj;
+  } else {
+    var isArray = Array.isArray(obj);
+    var props = [];
+    var open = isArray ? '[' : '{';
+    var close = isArray ? ']' : '}';
+    each(obj, function(value, key) {
+      key = isArray ? '' : stringifyJSON(key) + ':';
+      value = stringifyJSON(value);
+      if(!(!isArray && value === undefined)) {
+        props.push(key + value);
+      }
+    });
+    return open + props.join(',') + close;
+  }
 };
