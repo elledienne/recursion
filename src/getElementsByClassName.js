f65f998788c1c168175da2ae496d2ed629613fdd
Lorenzo De Nobili <elledienne@gmail.com>
2015-09-21 08:17:34 +0200

    Small code refactoring

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,9 +6,7 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className){
   var nodesWithClass = [];
-  var node
-  var passedNode = Array.prototype.slice.call(arguments, 1)[0];
-  node = passedNode !== undefined ? passedNode : document.body;
+  var node = Array.prototype.slice.call(arguments, 1)[0] || document.body;
   if(node.classList && node.classList.contains(className)) {
     nodesWithClass.push(node);  
   }
